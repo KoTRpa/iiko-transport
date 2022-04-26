cc72e33f223f06ef3a9e9861856b7a508bf5ec57
Max Kashkin <4kotrpa@gmail.com>
2022-04-27 02:25:57 +0800

    quick checker for required fileds

---
+++ src/Contracts/RequiredFields.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace KMA\IikoTransport\Contracts;
+
+use KMA\IikoTransport\Exceptions\MissingRequiredFieldException;
+
+trait RequiredFields
+{
+    /**
+     * @param array $source
+     * @return void
+     * @throws MissingRequiredFieldException
+     */
+    public function validateRequiredFields(array $source)
+    {
+        foreach ($this->requiredFields() as $r) {
+            if (!isset($source[$r])) {
+                throw new MissingRequiredFieldException($r);
+            }
+        }
+    }
+
+    protected function requiredFields(): array
+    {
+        return [];
+    }
+}
--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -5,11 +5,13 @@ namespace KMA\IikoTransport\Entities;
 use KMA\IikoTransport\Contracts\IEntity;
 use KMA\IikoTransport\Contracts\Arrayable;
 use KMA\IikoTransport\Contracts\Jsonable;
+use KMA\IikoTransport\Contracts\RequiredFields;
 
 abstract class Entity implements \JsonSerializable, IEntity
 {
     use Arrayable;
     use Jsonable;
+    use RequiredFields;
 
     /**
      * @inheritDoc
