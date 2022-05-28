cfd458668682fac4fbbc976ee7669464cc9e28c8
Max Kashkin <4kotrpa@gmail.com>
2022-05-28 12:31:06 +0800

    refactor delivery endpoint tests

--- tests/Endpoints/DeliveryTest.php
+++ tests/Endpoints/DeliveryTest.php
@@ -3,18 +3,13 @@
 namespace KMA\IikoTransport\Tests\Endpoints;
 
 use KMA\IikoTransport\Tests\EndpointTestCase;
-use KMA\IikoTransport\Tests\JsonFactory;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Middleware;
-use GuzzleHttp\Psr7\Response;
-use KMA\IikoTransport\Entities\Delivery\Addresses\CitiesRequest;
-use KMA\IikoTransport\Entities\Delivery\Addresses\CitiesResponse;
+
 use KMA\IikoTransport\Entities\Delivery\CreateDelivery\CreateDeliveryRequest;
 use KMA\IikoTransport\Entities\Delivery\Response\CreateDeliveryResponse;
 use KMA\IikoTransport\Entities\Delivery\Retrieve\RetrieveByIdRequest;
 use KMA\IikoTransport\Entities\Delivery\Retrieve\RetrieveByIdResponse;
-use KMA\IikoTransport\IikoTransport;
+use KMA\IikoTransport\Entities\Delivery\Addresses\CitiesRequest;
+use KMA\IikoTransport\Entities\Delivery\Addresses\CitiesResponse;
 
 class DeliveryTest extends EndpointTestCase
 {
@@ -23,36 +18,13 @@ class DeliveryTest extends EndpointTestCase
      */
     public function testCreateDelivery()
     {
-        $jsonAuth = JsonFactory::all('auth');
-        $jsonCreateDelivery = JsonFactory::all('Delivery/createDeliveryResponse');
-
-        $mock = new MockHandler([
-            new Response(200, ['content-type' => 'application/json; charset=utf-8'], $jsonAuth),
-            new Response(200, ['content-type' => 'application/json; charset=utf-8'], $jsonCreateDelivery),
-        ]);
-
-        $handlerStack = HandlerStack::create($mock);
-
-        $requestHistory = [];
-        $handlerStack->push(Middleware::history($requestHistory));
-
-        $iiko = new IikoTransport([
-            'url' => '/',
-            'login' => 'demo',
-            'http' => [
-                'handler' => $handlerStack
-            ]
-        ]);
-
-        $response = $iiko->createDelivery(new CreateDeliveryRequest);
-
-        $this->assertCount(2, $requestHistory);
-
-        $this->assertEquals('/api/1/deliveries/create', $requestHistory[1]['request']->getUri()->getPath());
-        $this->assertEquals(200, $requestHistory[1]['response']->getStatusCode());
-
-        $this->assertInstanceOf(CreateDeliveryResponse::class, $response);
-        $this->assertEquals(CreateDeliveryResponse::fromJson($jsonCreateDelivery), $response);
+        $this->runTests(
+            'Delivery/CreateDeliveryResponse',
+            'createDelivery',
+            CreateDeliveryRequest::class,
+            CreateDeliveryResponse::class,
+            '/api/1/deliveries/create'
+        );
     }
 
     /**
@@ -60,36 +32,13 @@ class DeliveryTest extends EndpointTestCase
      */
     public function testRetrieveById()
     {
-        $jsonAuth = JsonFactory::all('auth');
-        $json = JsonFactory::all('Delivery/RetrieveByIdResponse');
-
-        $mock = new MockHandler([
-            new Response(200, ['content-type' => 'application/json; charset=utf-8'], $jsonAuth),
-            new Response(200, ['content-type' => 'application/json; charset=utf-8'], $json),
-        ]);
-
-        $handlerStack = HandlerStack::create($mock);
-
-        $requestHistory = [];
-        $handlerStack->push(Middleware::history($requestHistory));
-
-        $iiko = new IikoTransport([
-            'url' => '/',
-            'login' => 'demo',
-            'http' => [
-                'handler' => $handlerStack
-            ]
-        ]);
-
-        $response = $iiko->retrieveById(new RetrieveByIdRequest);
-
-        $this->assertCount(2, $requestHistory);
-
-        $this->assertEquals('/api/1/deliveries/by_id', $requestHistory[1]['request']->getUri()->getPath());
-        $this->assertEquals(200, $requestHistory[1]['response']->getStatusCode());
-
-        $this->assertInstanceOf(RetrieveByIdResponse::class, $response);
-        $this->assertEquals(RetrieveByIdResponse::fromJson($json), $response);
+        $this->runTests(
+            'Delivery/RetrieveByIdResponse',
+            'retrieveById',
+            RetrieveByIdRequest::class,
+            RetrieveByIdResponse::class,
+            '/api/1/deliveries/by_id'
+        );
     }
 
     /**
